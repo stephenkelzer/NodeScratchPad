a11db95ef7e6a65d7bfe30a1d02550c71a929fbb
Stephen Kelzer <stephen@teamgenius.com>
2022-03-11 19:52:59 -0600

    efficiency tweak

--- src/script.ts
+++ src/script.ts
@@ -5,13 +5,23 @@ import { padRight } from './padRight'
 axios
   .get<IAstroResponse>('http://api.open-notify.org/astros.json')
   .then(({ data }) => {
-    const nameMaxLength = Math.max(...data.people.map((x) => x.name.length))
-    const craftMaxLength = Math.max(...data.people.map((x) => x.craft.length))
+    let nameMaxLength = 0
+    let craftMaxLength = 0
+
+    data.people.forEach((x) => {
+      if (x.name.length > nameMaxLength) {
+        nameMaxLength = x.name.length
+      }
+      if (x.craft.length > craftMaxLength) {
+        craftMaxLength = x.craft.length
+      }
+    })
 
     console.log(`There are ${data.people.length} people in space right now:`)
     console.log('')
     console.log(`${padRight('Name', nameMaxLength, ' ')} | ${'Craft'}`)
     console.log(`${padRight('', nameMaxLength, '-')} | ${padRight('', craftMaxLength, '-')}`)
+
     let lastCraft = null
     for (const { craft, name } of data.people) {
       let craftDisplay = ''
