ffe923358c3aba674e98f678cf6f500e837e1d18
Stephen Kelzer <stephen@teamgenius.com>
2022-03-11 18:17:36 -0600

    Finally! (ES2017)

---
+++ src/padRight.ts
@@ -0,0 +1,15 @@
+export const padRight = (
+  value: string,
+  desiredLength: number,
+  padChar: string
+): string => {
+  if (value.length <= desiredLength) {
+    let returnValue = value;
+    for (let index = value.length; index < desiredLength; index++) {
+      returnValue += padChar;
+    }
+    return returnValue;
+  } else {
+    return value.substring(0, desiredLength);
+  }
+};
--- src/script.ts
+++ src/script.ts
@@ -1,43 +1,16 @@
-import fetch from "node-fetch";
+import axios from "axios";
+import { IAstroResponse } from "./IAstroResponse";
+import { padRight } from "./padRight";
 
-export interface IAstroResponse {
-  people: Array<{ craft: string; name: string }>;
-  message: string;
-  number: number;
-}
+axios
+  .get<IAstroResponse>("http://api.open-notify.org/astros.json")
+  .then(({ data }) => {
+    const nameMaxLength = Math.max(...data.people.map((x) => x.name.length));
+    const craftMaxLength = Math.max(...data.people.map((x) => x.craft.length));
 
-const padRight = (
-  value: string,
-  desiredLength: number,
-  padChar: string
-): string => {
-  if (value.length <= desiredLength) {
-    let returnValue = value;
-    for (let index = value.length; index < desiredLength; index++) {
-      returnValue += padChar;
-    }
-    return returnValue;
-  } else {
-    return value.substring(0, desiredLength);
-  }
-};
-
-fetch("http://api.open-notify.org/astros.json", {
-  method: "GET",
-})
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(`Failed Request! Status Code: ${res.statusText}`);
-    }
-    return res.json();
-  })
-  .then((res: IAstroResponse) => {
-    const nameMaxLength = Math.max(...res.people.map((x) => x.name.length));
-    const craftMaxLength = Math.max(...res.people.map((x) => x.craft.length));
-
-    console.log(`There are ${res.people.length} people in space right now:`);
+    console.log(`There are ${data.people.length} people in space right now:`);
     console.log("");
-    console.log(`${"Name".padEnd(nameMaxLength, " ")} | ${"Craft"}`);
+    console.log(`${padRight("Name", nameMaxLength, " ")} | ${"Craft"}`);
     console.log(
       `${padRight("", nameMaxLength, "-")} | ${padRight(
         "",
@@ -46,7 +19,7 @@ fetch("http://api.open-notify.org/astros.json", {
       )}`
     );
     let lastCraft = null;
-    for (const { craft, name } of res.people) {
+    for (const { craft, name } of data.people) {
       let craftDisplay = "";
       if (lastCraft !== craft) {
         craftDisplay = craft;
@@ -56,4 +29,4 @@ fetch("http://api.open-notify.org/astros.json", {
       console.log(`${padRight(name, nameMaxLength, " ")} | ${craftDisplay}`);
     }
   })
-  .catch((err) => console.log(err));
+  .catch((err: Error) => console.log(err));
